d76a6124f84531b398de5642ec82ff8dc86864cf
Andres-0620 <afduranc@ufpso.edu.co>
2025-11-23 16:57:14 -0500

    Add files via upload

---
+++ src/Nombre.java
@@ -0,0 +1,18 @@
+class Persona {
+    String nombre;
+}
+
+public class Nombre {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.nombre = "Juan";
+        cambiarNombre(persona);
+        System.out.println("Después de cambiarNombre: " + persona.nombre);  // Imprime Pedro
+    }
+
+    public static void cambiarNombre(Persona persona) {
+        persona.nombre = "Pedro";
+        System.out.println("Dentro de cambiarNombre: " + persona.nombre);  // Imprime Pedro
+    }
+}
+
